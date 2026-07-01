ef10c4829da577d2e5a3653b39ae270a76bd7736
lin <ysjjovo@163.com>
2026-07-01 17:28:00 +0800

    add lsps

--- init.lua
+++ init.lua
@@ -693,15 +693,14 @@ do
   ---@type table<string, vim.lsp.Config>
   local servers = {
     -- clangd = {},
-    -- gopls = {},
-    -- pyright = {},
-    -- rust_analyzer = {},
-    --
+    gopls = {},
+    pyright = {},
+    rust_analyzer = {},
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
+    ts_ls = {},
 
     stylua = {}, -- Used to format Lua code
 
