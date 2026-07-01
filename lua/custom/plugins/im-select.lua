735a996c39c8b39355b1feb5248a04467e420d88
lin <ysjjovo@163.com>
2026-07-01 18:55:22 +0800

    fix im select

--- lua/custom/plugins/im-select.lua
+++ lua/custom/plugins/im-select.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_autocmd('InsertLeave', {
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinLeave', 'FocusLost' }, {
   callback = function()
     vim.fn.jobstart('im-select com.apple.keylayout.ABC')
   end,
