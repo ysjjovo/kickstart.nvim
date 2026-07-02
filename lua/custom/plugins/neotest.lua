f059defaa52ddc7b502b0dc9f23d45ebcdea171f
lin <ysjjovo@163.com>
2026-07-02 23:21:55 +0800

    add neotest

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,42 @@
+-- neotest: 测试运行框架，支持光标处跑方法、类跑整组、dap 调试
+-- https://github.com/nvim-neotest/neotest
+
+vim.pack.add {
+  'https://github.com/nvim-neotest/neotest',
+  'https://github.com/nvim-neotest/nvim-nio',   -- neotest 依赖（debug.lua 已装，幂等）
+  'https://github.com/nvim-neotest/neotest-python',
+}
+
+require('neotest').setup {
+  adapters = {
+    require('neotest-python') {
+      dap = { justMyCode = false },
+      runner = 'pytest',
+      python = function()
+        return vim.fn.getcwd() .. '/.venv/bin/python'
+      end,
+    },
+  },
+}
+
+local nt = require 'neotest'
+
+-- <leader>tr  光标处的方法（最常用）
+vim.keymap.set('n', '<leader>tr', function() nt.run.run() end,
+  { desc = '[T]est [R]un nearest' })
+
+-- <leader>tf  当前文件 / 光标在类名上时跑整个类
+vim.keymap.set('n', '<leader>tf', function() nt.run.run(vim.fn.expand '%') end,
+  { desc = '[T]est run [F]ile' })
+
+-- <leader>td  以 dap 模式调试光标处的方法（需要 venv 装了 debugpy）
+vim.keymap.set('n', '<leader>td', function() nt.run.run { strategy = 'dap' } end,
+  { desc = '[T]est [D]ebug nearest' })
+
+-- <leader>ts  切换测试结果面板
+vim.keymap.set('n', '<leader>ts', function() nt.summary.toggle() end,
+  { desc = '[T]est [S]ummary toggle' })
+
+-- <leader>to  打开最近一次测试输出
+vim.keymap.set('n', '<leader>to', function() nt.output.open { enter = true } end,
+  { desc = '[T]est [O]utput open' })
