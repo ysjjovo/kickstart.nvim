5da064c7d4113b02bf69e86b7eb0cbd7bdd37346
lin <ysjjovo@163.com>
2026-07-10 02:40:23 +0800

    revert terminal windows move

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -43,7 +43,7 @@ vim.api.nvim_create_autocmd('TermOpen', {
   callback = function(ev)
     local buf = ev.buf
     local opts = { buffer = buf, nowait = true }
-    vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>',       opts)
+    --vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>',       opts)
     vim.keymap.set('t', '<C-h>',      '<Cmd>wincmd h<CR>', opts)
     vim.keymap.set('t', '<C-j>',      '<Cmd>wincmd j<CR>', opts)
     vim.keymap.set('t', '<C-k>',      '<Cmd>wincmd k<CR>', opts)
