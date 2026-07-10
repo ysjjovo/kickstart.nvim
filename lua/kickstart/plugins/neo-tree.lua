fecaf9b17b4d8be84c737c5ebfd890b258f054ab
lin <ysjjovo@163.com>
2026-07-11 00:48:45 +0800

    neo-tree cf/ca/cr

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,14 +11,16 @@ vim.keymap.set('n', 'f', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', s
 
 require('neo-tree').setup {
   filesystem = {
+    follow_current_file = { enabled = true },
     filtered_items = {
       visible = true, -- 被过滤的项会以灰色显示而非完全隐藏
-      hide_dotfiles = false, -- 显示以 . 开头的隐藏文件
+      hide_dotfiles = true, -- 按 H 切换显示/隐藏
       hide_gitignored = false, -- 显示被 .gitignore 忽略的文件
     },
     window = {
       mappings = {
         ['q'] = 'close_window',
+        ['c'] = { 'show_help', nowait = false, config = { title = 'Copy', prefix_key = 'c' } },
         ['ca'] = {
           function(state)
             local node = state.tree:get_node()
@@ -27,6 +29,15 @@ require('neo-tree').setup {
             vim.notify('Copied path: ' .. path)
           end,
           desc = 'Copy Absolute Path',
+        },
+        ['cf'] = {
+          function(state)
+            local node = state.tree:get_node()
+            local filename = vim.fn.fnamemodify(node:get_id(), ':t')
+            vim.fn.setreg('+', filename, 'c')
+            vim.notify('Copied filename: ' .. filename)
+          end,
+          desc = 'Copy Filename',
         },
         ['cr'] = {
           function(state)
