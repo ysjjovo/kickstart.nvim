98a68fa30be7db198edf318e38cf043f093d0a0d
lin <ysjjovo@163.com>
2026-07-13 01:51:14 +0800

    custom theme

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+local theme = 'catppuccin' -- 'catppuccin' | 'tokyonight' | 'onedark'
+
+local themes = {
+  catppuccin = function()
+    vim.pack.add { 'https://github.com/catppuccin/nvim' }
+    require('catppuccin').setup { default_integrations = false }
+    vim.cmd.colorscheme 'catppuccin-mocha'
+  end,
+  tokyonight = function()
+    vim.pack.add { 'https://github.com/folke/tokyonight.nvim' }
+    vim.cmd.colorscheme 'tokyonight-night'
+  end,
+  onedark = function()
+    vim.pack.add { 'https://github.com/olimorris/onedarkpro.nvim' }
+    vim.cmd.colorscheme 'onedark'
+  end,
+}
+
+themes[theme]()
