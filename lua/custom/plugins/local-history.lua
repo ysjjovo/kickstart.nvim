06ccca81fa455935e0b85531fb7183dd8672ea04
lin <ysjjovo@163.com>
2026-07-13 01:55:47 +0800

    local history plugin

--- lua/custom/plugins/local-history.lua
+++ lua/custom/plugins/local-history.lua
@@ -5,9 +5,9 @@ vim.pack.add {
   'https://github.com/dinhhuy258/vim-local-history',
 }
 
-vim.g.local_history_path = vim.fn.expand '~/.local-history'
-vim.g.local_history_max_changes = 100
-vim.g.local_history_new_change_delay = 300
-vim.g.local_history_exclude = { '**/node_modules/**', '**/.git/**' }
+-- vim.g.local_history_path = vim.fn.expand '~/.local-history'
+-- vim.g.local_history_max_changes = 100
+-- vim.g.local_history_new_change_delay = 300
+vim.g.local_history_exclude = { '**/node_modules/**', '**/.git/**', '**/.venv/**', '**/venv/**', '**/__pycache__/**', '**/*.pyc' }
 
-vim.keymap.set('n', '<leader>lh', '<Cmd>LocalHistoryToggle<CR>', { desc = '[L]ocal [H]istory' })
+vim.keymap.set('n', '<leader>ul', '<Cmd>LocalHistoryToggle<CR>', { desc = 'Toogle [L]ocal history' })
