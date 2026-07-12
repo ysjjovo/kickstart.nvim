f36898a59f6ff30ce2758f0fb799fe9d047829a4
lin <ysjjovo@163.com>
2026-07-13 01:10:04 +0800

    add lualine

---
+++ lua/custom/plugins/local-history.lua
@@ -0,0 +1,13 @@
+-- vim-local-history: file local history on save
+-- https://github.com/dinhhuy258/vim-local-history
+
+vim.pack.add {
+  'https://github.com/dinhhuy258/vim-local-history',
+}
+
+vim.g.local_history_path = vim.fn.expand '~/.local-history'
+vim.g.local_history_max_changes = 100
+vim.g.local_history_new_change_delay = 300
+vim.g.local_history_exclude = { '**/node_modules/**', '**/.git/**' }
+
+vim.keymap.set('n', '<leader>lh', '<Cmd>LocalHistoryToggle<CR>', { desc = '[L]ocal [H]istory' })
---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,8 @@
+-- lualine.nvim: statusline
+-- https://github.com/nvim-lualine/lualine.nvim
+
+vim.pack.add {
+  'https://github.com/nvim-lualine/lualine.nvim',
+}
+
+require('lualine').setup()
