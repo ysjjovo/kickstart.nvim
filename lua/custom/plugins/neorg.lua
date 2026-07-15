a003986902781f69d7243e9cf5a91ae87913fc0a
lin <ysjjovo@163.com>
2026-07-15 23:01:28 +0800

    yank password

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -37,3 +37,29 @@ end, { desc = 'Neorg find file' })
 vim.keymap.set('n', '<leader>ng', function()
   require('telescope.builtin').live_grep { cwd = notes_dir }
 end, { desc = 'Neorg grep content' })
+
+local ns = vim.api.nvim_create_namespace("neorg_pw")
+
+local function conceal_pw(buf)
+    vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+    for i, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+        local _, e = line:find("Password:%s+")
+        if e then
+            vim.api.nvim_buf_set_extmark(buf, ns, i - 1, e, { end_col = #line, conceal = "*" })
+        end
+    end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "norg",
+    callback = function(ev)
+        vim.opt_local.conceallevel = 2
+        vim.opt_local.concealcursor = "nv"
+        conceal_pw(ev.buf)
+        vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, { buffer = ev.buf, callback = function() conceal_pw(ev.buf) end })
+    end,
+})
+vim.keymap.set("n", "<leader>yp", function()
+    local pw = vim.api.nvim_get_current_line():match("Password:%s+(.*)")
+    if pw then vim.fn.setreg("+", pw) end
+end, { desc = 'Yank [P]assword' })
