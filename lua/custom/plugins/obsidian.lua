0fd8fede74c1bd86eab006c82fad0ded12a0f3ed
lin <ysjjovo@163.com>
2026-07-17 16:22:43 +0800

    obsidian plugin

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -14,12 +14,44 @@ require('obsidian').setup {
   end,
 }
 
+-- password concealing in obsidian vault
+local pw_ns = vim.api.nvim_create_namespace('obsidian_pw')
+local vault_path = vim.fn.resolve(vim.fn.expand('~/notes/obsidian'))
+
+local function conceal_pw(buf)
+  vim.api.nvim_buf_clear_namespace(buf, pw_ns, 0, -1)
+  for i, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+    local _, e = line:find('[Pp]assword:%s+')
+    if e then
+      vim.api.nvim_buf_set_extmark(buf, pw_ns, i - 1, e, { end_col = #line, conceal = '*' })
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = vim.fn.expand('~/notes/obsidian') .. '/**',
+  callback = function(ev)
+    vim.opt_local.conceallevel = 2
+    vim.opt_local.concealcursor = 'nv'
+    conceal_pw(ev.buf)
+    vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI' }, {
+      buffer = ev.buf,
+      callback = function() conceal_pw(ev.buf) end,
+    })
+  end,
+})
+
 local map = vim.keymap.set
-map('n', '<leader>nn', '<cmd>ObsidianNew<cr>', { desc = 'New note' })
-map('n', '<leader>nf', '<cmd>ObsidianQuickSwitch<cr>', { desc = 'Find note' })
-map('n', '<leader>ns', '<cmd>ObsidianSearch<cr>', { desc = 'Search in notes' })
-map('n', '<leader>nb', '<cmd>ObsidianBacklinks<cr>', { desc = 'Backlinks' })
-map('n', '<leader>nd', '<cmd>ObsidianToday<cr>', { desc = 'Daily note' })
-map('n', '<leader>nl', '<cmd>ObsidianFollowLink<cr>', { desc = 'Follow link' })
-map('n', '<leader>nt', '<cmd>ObsidianTags<cr>', { desc = 'Tags' })
-map('n', '<leader>nr', '<cmd>ObsidianRename<cr>', { desc = 'Rename note' })
+map('n', '<leader>yp', function()
+  local line = vim.api.nvim_get_current_line()
+  local pw = line:match('[Pp]assword:%s+(.*)')
+  if pw then vim.fn.setreg('+', pw) end
+end, { desc = 'Yank [P]assword' })
+map('n', '<leader>nn', '<cmd>ObsidianNew<cr>', { desc = '[N]ew note' })
+map('n', '<leader>nf', '<cmd>ObsidianQuickSwitch<cr>', { desc = '[F]ind note' })
+map('n', '<leader>ng', '<cmd>ObsidianSearch<cr>', { desc = '[Grep] in notes' })
+map('n', '<leader>nb', '<cmd>ObsidianBacklinks<cr>', { desc = '[B]acklinks' })
+map('n', '<leader>nd', '<cmd>ObsidianToday<cr>', { desc = '[D]aily note' })
+map('n', '<leader>nl', '<cmd>ObsidianFollowLink<cr>', { desc = '[F]ollow link' })
+map('n', '<leader>nt', '<cmd>ObsidianTags<cr>', { desc = '[T]ags' })
+map('n', '<leader>nr', '<cmd>ObsidianRename<cr>', { desc = '[R]ename note' })
