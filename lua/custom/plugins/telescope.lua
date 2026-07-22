134b8ac02636f0e5f5b37a4441de037dd4f06f23
lin <ysjjovo@163.com>
2026-07-23 00:48:41 +0800

    remap telescope keys

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -36,8 +36,8 @@ vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect T
 vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch Resume ("." for resume)' })
+vim.keymap.set('n', '<leader>sr', builtin.oldfiles, { desc = '[S]earch [R]ecent Files' })
 vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
 vim.keymap.set('n', '<leader>se', builtin.buffers, { desc = '[S]earch [E]xisting buffers' })
 
