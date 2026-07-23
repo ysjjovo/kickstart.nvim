c19949acb4ef0325b8fc65ddd438a0d4aadf554a
lin <ysjjovo@163.com>
2026-07-24 01:29:10 +0800

    fix which key group

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -31,7 +31,7 @@ pcall(require('telescope').load_extension, 'ui-select')
 local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[S]earch []files' })
+vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Search files' })
 vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
