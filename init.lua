d7a0ff7f775b83edc174c1814bd2a1713126d943
lin <ysjjovo@163.com>
2026-07-25 01:28:40 +0800

    no hint in the line end

--- init.lua
+++ init.lua
@@ -144,7 +144,7 @@ do
     underline = { severity = { min = vim.diagnostic.severity.WARN } },
 
     -- Can switch between these as you prefer
-    virtual_text = true, -- Text shows up at the end of the line
+    virtual_text = false, -- Text shows up at the end of the line
     virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
     -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
