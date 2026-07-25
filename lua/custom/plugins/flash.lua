6754d222f9527d5aafb6bed200d69dcd1043ba2f
lin <ysjjovo@163.com>
2026-07-26 01:17:23 +0800

    flash pink jump

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -9,6 +9,9 @@ require('flash').setup {
   },
 }
 
+-- 让跳转标签更醒目：亮色前景 + 加粗
+vim.api.nvim_set_hl(0, 'FlashLabel', { fg = '#ff007c', bold = true })
+
 vim.keymap.set({ 'n', 'x', 'o' }, 'f', function() require('flash').jump() end, { desc = 'Flash Jump' })
 vim.keymap.set({ 'n', 'x', 'o' }, 'F', function() require('flash').treesitter() end, { desc = 'Flash Treesitter' })
 
