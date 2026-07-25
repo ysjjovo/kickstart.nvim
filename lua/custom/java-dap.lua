c5132683e59b75b5095b604defa677ed521daf00
lin <ysjjovo@163.com>
2026-07-25 22:07:27 +0800

    optimize java debug startup

--- lua/custom/java-dap.lua
+++ lua/custom/java-dap.lua
@@ -27,6 +27,28 @@ function M.launch(no_debug)
   end
 
   require('dapui').open()
+
+  -- 持续显示启动状态，直到第一行输出
+  local msg = (no_debug and 'Running ' or 'Debugging ') .. fqcn .. '…'
+  local timer = vim.uv.new_timer()
+  local dots = 0
+  timer:start(0, 800, vim.schedule_wrap(function()
+    dots = (dots + 1) % 4
+    vim.api.nvim_echo({ { msg .. string.rep('.', dots), 'DiagnosticInfo' } }, false, {})
+  end))
+  local function stop_timer()
+    if timer and timer:is_active() then timer:stop() timer:close() end
+    vim.api.nvim_echo({ { '' } }, false, {})
+  end
+  dap.listeners.before.event_output['startup_notify'] = function()
+    stop_timer()
+    dap.listeners.before.event_output['startup_notify'] = nil
+  end
+  dap.listeners.before.event_terminated['startup_notify'] = function()
+    stop_timer()
+    dap.listeners.before.event_terminated['startup_notify'] = nil
+  end
+
   dap.run {
     type = 'java',
     request = 'launch',
@@ -36,6 +58,8 @@ function M.launch(no_debug)
     console = 'internalConsole',
     shortenCommandLine = 'argfile',
     noDebug = no_debug or nil,
+    -- 加速 JVM 启动：跳过 C2 编译，禁用字节码验证
+    vmArgs = '-XX:TieredStopAtLevel=1 -noverify',
   }
 end
 
