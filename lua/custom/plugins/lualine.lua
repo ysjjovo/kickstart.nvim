6ea6deb62bca270e53b61e0cb304049ae01dccc2
lin <ysjjovo@163.com>
2026-07-26 01:03:03 +0800

    lualine diplay unsave file

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -10,8 +10,25 @@ vim.o.showtabline = 0
 
 require('lualine').setup {
   sections = {
-    lualine_c = { { 'filename', path = 3, symbols = { modified = ' ●' } } },
-    -- 多 tab 时在右下角显示页码
-    lualine_z = { { 'tabs', mode = 0, show_modified_status = false } },
+    lualine_c = {
+      {
+        'filename',
+        path = 3,
+        -- symbols = { modified = '' },
+        -- 未保存时整个文件名变橙色，提醒保存
+        color = function()
+          if vim.bo.modified then return { fg = '#ff9e64' } end
+        end,
+      },
+    },
+    -- 多 tab 时在右下角显示页码，modified tab 带 ● 标记
+    lualine_z = {
+      {
+        'tabs',
+        mode = 0,
+        show_modified_status = true,
+        -- symbols = { modified = ' ●' },
+      },
+    },
   },
 }
